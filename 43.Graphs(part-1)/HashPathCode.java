ec29cf4fdc61f0c6bb89750ab3c86552d65229df
Mr_Shubham_Khalane <118313700+1khalaneshubham@users.noreply.github.com>
2024-03-01 15:30:26 +0530

    Add files via upload

---
+++ 43.Graphs(part-1)/HashPathCode.java
@@ -0,0 +1,112 @@
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class HashPathCode {
+ 
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+    
+        public Edge(int s,int d,int w){
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+    static void CreateGraph(AbstractList<Edge> graph[]){
+        for(int i = 0; i<graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+            graph[0].add(new Edge(0, 1, 1));
+            graph[0].add(new Edge(0, 2, 1));
+
+            graph[1].add(new Edge(1, 0, 1));
+            graph[1].add(new Edge(1, 3, 1));
+
+
+            graph[2].add(new Edge(2, 0, 1));
+            graph[2].add(new Edge(2, 4, 1));
+
+            graph[3].add(new Edge(3, 1, 1));
+            graph[3].add(new Edge(3, 4, 1));
+            graph[3].add(new Edge(3, 5, 1));
+
+            graph[4].add(new Edge(4, 2, 1)); 
+            graph[4].add(new Edge(4, 3, 1));
+            graph[4].add(new Edge(4, 5, 1));
+
+            graph[5].add(new Edge(5, 3, 1)); 
+            graph[5].add(new Edge(5, 4, 1));
+            graph[5].add(new Edge(5, 6, 1));
+
+            graph[5].add(new Edge(6, 5, 1));
+    }
+
+    public static void BSF(ArrayList<Edge>[] graph){  // TC-O(n)
+        Queue<Integer> q = new LinkedList<>();
+        boolean vis[] = new boolean[graph.length];
+        q.add(0);
+
+        while(!q.isEmpty()) {
+            int curr = q.remove();
+
+            if(!vis[curr]){ // visit curr
+                System.out.print(curr+" ");
+                vis[curr] = true;
+                for(int i=0; i<graph[curr].size(); i++){
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
+
+    public static void DSF(ArrayList<Edge>[] graph, int curr, boolean vis[]){
+        // visit
+        System.out.print(curr + " ");
+        vis[curr] = true;
+
+        for(int i = 0; i<graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if(!vis[e.dest]){
+                DSF(graph, e.dest, vis);
+            }
+        }
+    }
+
+    public static boolean HashPath(ArrayList<Edge>[] graph, int src,int dest,boolean vis[]){
+
+        if(src == dest){
+            return true;
+        }
+        vis[src] = true;
+        for(int i = 0;  i<graph[src].size(); i++){
+            Edge e = graph[src].get(i);
+            // e.dest = neighbour
+            if(!vis[e.dest] && HashPath(graph,e.dest,dest,vis)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        
+        /*     1---3
+         *   /    | \
+         * 0      |  5 -- 6
+         *  \    | /
+         *  2---4
+         * 
+        */
+        int V = 7;
+
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        CreateGraph(graph);
+        
+        System.out.println(HashPath(graph,0,5, new boolean[V]));
+    }
+}
